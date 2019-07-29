884f9c5b1166e51a2df9fdb3129897cc91583cc4
Nicole Marty <nmarty310@gmail.com>
2019-07-29 12:58:26 -0500

    added in question, choices, and answer with image content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,5 +3,29 @@ $("#start").on("click", function() {
 })
 
 var questions = [{
-    question: choices: answer: image:
+    question: "What is Wayne's best friend's last name?",
+    choices: ["Campbell, Smith, Algar, McKeeta"],
+    answer: "Algar",
+    image: ""
+}, {
+    question: "What does Wayne spend his tv show cash on?",
+    choices: ["Mustang, concert tickets, all the pizza, Fender Stratacaster"],
+    answer: "Fender Stratacaster",
+    image: ""
+}, {
+    question: "What rockstar do Wayne and Garth deem themselves to be unworthy of?",
+    choices: ["Alice Cooper, Jim Morrison, Freddie Mercury, Robert Plant"],
+    answer: "Alice Cooper",
+    image: ""
+}, {
+    question: "What gift does Wayne's psychotic ex-girlfriend give to him?",
+    choices: "a guitar, a gun rack, a Ford Pinto, new kicks",
+    answer: "a gun rack",
+    image: ""
+}, {
+    question: "What song does Wayne always hear in his head when he sees Cassandra?",
+    choices: "Foxy, Dreamweaver, Feed My Frankenstein, Bohemian Rhapsody",
+    answer: "Dreamweaver",
+    image: ""
+
 }]
