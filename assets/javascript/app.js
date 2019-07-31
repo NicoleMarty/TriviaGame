754d0efc60594e3d73257f01d2521954b412fc30
Nicole Marty <nmarty310@gmail.com>
2019-07-31 17:34:08 -0500

    keep getting bummer in console even when click correct answer. Not sure why. to be continued...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,31 +3,35 @@ $("#start").on("click", function() {
     game.renderQuestion(); // render question
 })
 
+$(document).on("click", ".answer-button", function(e) {
+    game.clicked(e);
+})
+
 // VARIABLES
 // define object array for each question (var questions)
 var questions = [{
     question: "What is Wayne's best friend's last name?",
-    choices: ["Campbell, Smith, Algar, McKeeta"],
+    choices: ["Campbell", "Smith", "Algar", "McKeeta"],
     answer: "Algar",
     image: ""
 }, {
     question: "What does Wayne spend his tv show cash on?",
-    choices: ["Mustang, concert tickets, all the pizza, Fender Stratacaster"],
+    choices: ["Mustang", "concert tickets", "all the pizza", "Fender Stratacaster"],
     answer: "Fender Stratacaster",
     image: ""
 }, {
     question: "What rockstar do Wayne and Garth deem themselves to be unworthy of?",
-    choices: ["Alice Cooper, Jim Morrison, Freddie Mercury, Robert Plant"],
+    choices: ["Alice Cooper", "Jim Morrison", "Freddie Mercury", "Robert Plant"],
     answer: "Alice Cooper",
     image: ""
 }, {
     question: "What gift does Wayne's psychotic ex-girlfriend give to him?",
-    choices: "a guitar, a gun rack, a Ford Pinto, new kicks",
+    choices: ["a guitar", "a gun rack", "a Ford Pinto", "new kicks"],
     answer: "a gun rack",
     image: ""
 }, {
     question: "What song does Wayne always hear in his head when he sees Cassandra?",
-    choices: "Foxy, Dreamweaver, Feed My Frankenstein, Bohemian Rhapsody",
+    choices: ["Foxy", "Dreamweaver", "Feed My Frankenstein", "Bohemian Rhapsody"],
     answer: "Dreamweaver",
     image: ""
 }];
@@ -35,7 +39,7 @@ var questions = [{
 // define objects of game components (var game)
 var game = {
     question: questions,
-    currentQuestion: 0, //what question on
+    currentQuestion: 0, //index number for what question is displaying currently
     counter: 30,
     correctAnswer: 0,
     incorrectAnswer: 0,
@@ -51,24 +55,30 @@ var game = {
         }
     },
     renderQuestion: function() {
-        timer = setInterval(game.countdown, 1000);
+        timer = setInterval(game.countdown, 1000); //sets timer when renderQuestion runs
         $("#subwrapper").html('<h2>' + questions[game.currentQuestion].question + '<h2>');
         for (var i = 0; i < questions[game.currentQuestion].choices.length; i++) {
-            // adding buttons for 
-            $("#subwrapper").append('<button class="answer-button" id="button-' + i + '" data-name="' +
-                questions[game.currentQuestion].choices[i] + '">' + questions[game.currentQuestion]
+            // adding buttons for choices
+            $("#subwrapper").append('<button class="answer-button" id="button-' + i + '" data-answer=" ' +
+                questions[game.currentQuestion].choices[i] + ' "> ' + questions[game.currentQuestion]
                 .choices[i] + '</button>');
         }
     },
-    nextquestion: function() {
-
-    },
     congratulations: function() {
-
+        console.log("PARTY ON!");
     },
     bummer: function() {
+        console.log("BUMMER...");
 
     },
+    nextquestion: function() {
+        game.counter = 30;
+        $("#counter").html(game.counter);
+        game.currentQuestion++;
+        game.renderQuestion();
+
+    },
+
     timesup: function() {
 
     },
@@ -78,7 +88,14 @@ var game = {
     results: function() {
 
     },
-    clicked: function() {
+    // e short for event to pass through what is clicked
+    clicked: function(e) {
+        clearInterval(timer);
+        if ($(e.target).data("answer") === questions[game.currentQuestion].answer) {
+            game.congratulations();
+        } else {
+            game.bummer();
+        }
 
     }
 
